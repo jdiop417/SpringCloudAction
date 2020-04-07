2568b78e41f17ecd47eadccaedbd7d6e2df430d5
yang.chen <jdiop417@gmail.com>
2020-04-07 09:15:09 +0800

    string 面试题

--- hystrix-collapser/src/main/java/org/learning/springboot/hystrix/convert/PlaceConvert.java
+++ hystrix-collapser/src/main/java/org/learning/springboot/hystrix/convert/PlaceConvert.java
@@ -20,4 +20,13 @@ public class PlaceConvert extends Converter<Place, DepartmentDTO> {
         place.setCode(dept.getId());
         return place;
     }
+
+    public static void main(String[] args) {
+        String str1 = "abc";
+        String str2 = new String("abc");
+        String str3 = str2.intern();
+        System.out.println(str1 == str2);
+        System.out.println(str2 == str3);
+        System.out.println(str1 == str3);
+    }
 }
